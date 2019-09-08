3432c93a2d9296dc7db14a1096b3e466b30a6fa2
Linzay <panlinlin1023@gmail.com>
2019-09-08 22:41:08 +1200

    compose the component

--- src/index.js
+++ src/index.js
@@ -2,23 +2,18 @@ import React from "react";
 import { render } from "react-dom";
 import "./index.css";
 import App from "./App";
-import SkiDayCounter from "./SkiDayCounter";
+
 import * as serviceWorker from "./serviceWorker";
+///how to compose the component
 
-const skiData = {
-  total: 50,
-  powder: 20,
-  backcountry: 10,
-  goal: 100
+const Library = () => {
+  return <div>Welcome to library</div>;
 };
-const lakeList = [
-  { id: 1, name: "lake A" },
-  { id: 2, name: "lake B" },
-  { id: 3, name: "lake C" }
-];
-render(<SkiDayCounter skiData={skiData} />, document.getElementById("root"));
+render(
+  <div>
+    <Library /> <Library />
+  </div>,
+  document.getElementById("root")
+);
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
