1780a8e229d03f3c10b1f8c21a21aaedcf815c49
Linzay <panlinlin1023@gmail.com>
2019-09-10 12:09:09 +1200

    Property type

--- src/index.js
+++ src/index.js
@@ -4,11 +4,15 @@ import "./index.css";
 import Library from "./Library.js";
 import FavoriteColorForm from "./FavoriteColorForm";
 import * as serviceWorker from "./serviceWorker";
+import propTypes from "prop-types";
 ///how to compose the component
 const books = [
   { title: "Hunger", author: "Roxane Gay", pages: 320 },
   { title: "The Sun Also Rise", author: "Ernest Hemingway", pages: 420 },
   { title: "Cat's Cradle", author: "Kurt Vonnegut", pages: 480 }
 ];
-render(<Library />, document.getElementById("root"));
+Library.propTypes = {
+  books: propTypes.array
+};
+render(<Library books={books} />, document.getElementById("root"));
 serviceWorker.unregister();
